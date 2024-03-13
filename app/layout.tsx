1a2fa260220a9c3c1a2c736bd97495d946c2dc8c
ndnhattan <ndnhattan911@gmail.com>
2024-03-13 12:00:43 +0700

    initial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Script from "next/script";
+import "bootstrap/dist/css/bootstrap.css";
+import "../node_modules/font-awesome/css/font-awesome.min.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,6 +20,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
+      <Script src="../lib/jquery/jquery.min.js"></Script>
+      <Script src="../lib/bootstrap/js/bootstrap.min.js"></Script>
+      <Script src="../js/main.js"></Script>
     </html>
   );
 }
